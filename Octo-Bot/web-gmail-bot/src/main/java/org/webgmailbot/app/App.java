c79a9b626046c46ab1ca31340b56a7b82a0b5d87
Aris Cahyadi Risdianto <aris.risdianto@gmail.com>
2022-11-02 09:15:21 +0800

    Adding the selenium driver and fix dependencies.

--- Octo-Bot/web-gmail-bot/src/main/java/org/webgmailbot/app/App.java
+++ Octo-Bot/web-gmail-bot/src/main/java/org/webgmailbot/app/App.java
@@ -1,7 +1,9 @@
 package org.webgmailbot.app;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import com.sun.tools.javac.Main;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,9 +13,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class App {
 
-    public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "path of chrome driver");
+    public static void main(String[] args) throws Exception {
+
+        System.setProperty("webdriver.chrome.driver", "Octo-Bot/web-gmail-bot/src/main/resources/drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
